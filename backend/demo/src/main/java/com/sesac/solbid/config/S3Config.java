bd5a077f638feb56ae58d0c8ddd36240131b3333
박재성 <yoki06161@naver.com>
2025-09-19 16:35:08 +0900

    refactor(security): CORS 설정을 SecurityConfig로 일원화 (#302)

--- backend/demo/src/main/java/com/sesac/solbid/config/S3Config.java
+++ backend/demo/src/main/java/com/sesac/solbid/config/S3Config.java
@@ -3,8 +3,6 @@ package com.sesac.solbid.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -13,7 +11,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-public class S3Config implements WebMvcConfigurer {
+public class S3Config {
 
     // properties에서 직접 주입 (env 아님 -> 후에 .env 파일로 수정 예정)
     @Value("${spring.cloud.aws.credentials.access-key}")
@@ -56,14 +54,5 @@ public class S3Config implements WebMvcConfigurer {
                 .region(region())
                 .credentialsProvider(staticCreds())
                 .build();
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
     }
 }
